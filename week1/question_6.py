034fbcf4bf429c0fff85bd7d450eee84f70d2bb2
Abdul kalam Shaik <39231568+abdulkalam1233@users.noreply.github.com>
2020-05-06 16:51:39 +0530

    Update question_6.py

--- week1/question_6.py
+++ week1/question_6.py
@@ -7,3 +7,15 @@ class Solution:
                 return i
         return -1
         
+# using Boyer-Moore Voting Algorithm
+class Solution:
+    def majorityElement(self, nums):
+        count = 0
+        candidate = None
+
+        for num in nums:
+            if count == 0:
+                candidate = num
+            count += (1 if num == candidate else -1)
+
+        return candidate
